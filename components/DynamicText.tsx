a911e02479c5b5b0e48baa8ae1b440f198ab9a21
linhduyit <linhduyit@gmail.com>
2022-03-01 19:17:45 +0900

    update Dynamic component: change DynamicText component to accept forwardRef, useImperativeHandle to expose changeValue function to parent. Also export ref interface under DynamicTextRefObject

--- components/DynamicText.tsx
+++ components/DynamicText.tsx
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { forwardRef, Ref, useImperativeHandle, useState } from "react";
 import { Heading } from "@chakra-ui/react";
 
-const DynamicText = () => {
+export interface DynamicTextRefObject {
+  changeValue: (newValue: string) => void,
+}
+
+const DynamicText = (props, ref: Ref<DynamicTextRefObject>) => {
   const [value, setValue] = useState("Random Text");
 
-  const changeValue = (newValue) => {
+  useImperativeHandle(ref, () => ({
+    changeValue
+  }));
+
+  const changeValue = (newValue: string): void => {
     setValue(newValue);
   };
 
@@ -13,4 +21,4 @@ const DynamicText = () => {
   </Heading>
 };
 
-export default DynamicText;
+export default forwardRef(DynamicText);
