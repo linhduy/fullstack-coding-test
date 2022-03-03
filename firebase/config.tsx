da84097c91e3d33de2e5d38d232d35a7f53249ba
linhduyit <linhduyit@gmail.com>
2022-03-03 16:01:20 +0900

    update firebaseConfig: add firestore databaseURL

--- firebase/config.tsx
+++ firebase/config.tsx
@@ -1,6 +1,7 @@
 const firebaseConfig = {
   apiKey: "AIzaSyCxamTjr49u6bGxWjkGkZY6ZyeDS76I1ME",
   authDomain: "fullstack-coding-test-8de0e.firebaseapp.com",
+  databaseURL: "https://fullstack-coding-test-8de0e-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "fullstack-coding-test-8de0e",
   storageBucket: "fullstack-coding-test-8de0e.appspot.com",
   messagingSenderId: "520714673631",
